15b792a6ae906ab06b35b1e0dae9af179be59904
Yoan <y.n.dorny@gmail.com>
2018-01-15 18:37:31 +0100

    11

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <span className="ofThe">
     <span className="of">of</span>
     <span className="the">the</span></span>Day</h1>
-    <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+    <h3 className="tagline"><span>{props.tagline}</span></h3>
     </header>
     )
 
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  gotToStore(event) {
+    event.preventDefault()
+    console.log("Url changed")
+    //1 - grab the text from the box
+    const storeId = this.storeInput.value;
+    console.log(`going to ${storeId}`)
+    //2 - transition from / to store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`)
+  }
+
   render() {
     return (
-// <div><p>Hello world</p><h1>yo</h1></div>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={(e) => this.gotToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit store → </button>
         </form>
 
       )
-
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
